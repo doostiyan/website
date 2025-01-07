fb8a5ffb337d4666c9f602ade7bdcfebf0820417
amirh <amirh9476@gmail.com>
2025-01-07 16:09:08 +0330

    Update home.html

--- shop/views.py
+++ shop/views.py
@@ -10,11 +10,13 @@ from shop.models import Product, Category
 class HomeView(View):
     def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
+        product = Product.objects.filter(available=True).order_by('-id').first()
         categories = Category.objects.filter(is_sub=False)
         if category_slug:
             category = Category.objects.get(slug=category_slug)
             products = products.filter(category=category)
-        return render(request, 'shop/index.html', {'products': products, 'categories': categories})
+        return render(request, 'shop/index.html', {'products': products, 'categories': categories
+                                                   , 'product': product})
 
 
 class ProductDetailView(View):
